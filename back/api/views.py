5aa6dd3b29fcc872905d19004146b181561d93cf
Egor <egor29302@yandex.ru>
2024-02-02 13:45:36 +0300

    Add new endpoints (download) add workflows

--- back/api/views.py
+++ back/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+from django.http import FileResponse
 
 from api.tasks import mod_file
 from files.models import File
@@ -8,6 +10,7 @@ from api.serializers import FileSerializer
 
 
 class UploadFilesView(APIView):
+    """Эндпоинт для загрузки файлов на сервер"""
     def post(self, request, *args, **kwargs) -> Response:
         file_serializer = FileSerializer(data=request.data)
 
@@ -20,7 +23,18 @@ class UploadFilesView(APIView):
 
 
 class GetFilesListView(APIView):
+    """Эндпоинт для получения списка всех файлов"""
     def get(self, request, *args, **kwargs):
         files = File.objects.all()
         serializer = FileSerializer(files, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class DownloadFileView(APIView):
+    """Эндпоинт для скачивания определенного файла"""
+    def get(self, request, file_id):
+        file_instance = get_object_or_404(File, id=file_id)
+        file_path = file_instance.file.path
+        response = FileResponse(open(file_path, 'rb'))
+        response['Content-Disposition'] = f'attachment; filename="{file_instance.file.name}"'
+        return response
